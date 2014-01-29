f48d1416fc91344c4b9d89976ed045e6efb52431
Chris P <sahib@online.de>
2014-01-29 19:01:36 +0100

    Added dumb update script for gh-pages

---
+++ publish_html.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+make html singlehtml && cp _build/*html /tmp -r;
+git checkout gh-pages
+rm *html -rf && cp /tmp/*html . -r
+git add *html
+git commit -am 'Automated update.'
+git checkout master
